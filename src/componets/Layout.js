4cfef1f50c5e8d84e5990298005908d23ac7bd90
Gregory Kallai <gerg.kallai@gmail.com>
2019-08-18 20:13:34 +0200

    Renders all the games pending, user can navigate to it's instance

--- src/componets/Layout.js
+++ src/componets/Layout.js
@@ -4,6 +4,7 @@ import { Route, Switch, BrowserRouter} from 'react-router-dom';
 import Board from '../containers/Board/Board';
 import HomeScreen from './HomeScreen/HomeScreen';
 import NewGame from './HomeScreen/NewGame/NewGame';
+import LoadGame from './HomeScreen/LoadGame/LoadGame';
 
 const Layout = () => {
   return (
@@ -12,6 +13,7 @@ const Layout = () => {
         <Route exact path='/' component={HomeScreen} />
         <Route path='/board/:id' component={Board}/>
         <Route path='/new-game' component={NewGame} />
+        <Route path='/load-game' component={LoadGame} />
       </Switch>
     </BrowserRouter>
   )
